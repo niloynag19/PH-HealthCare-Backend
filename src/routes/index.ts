05af80e868e5807ac2a242ced79d82ae650947c0
unknown <niloynag19@gmail.com>
2026-02-22 12:35:30 +0600

    complete the schedule and doctor schedule

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,8 @@ import { AuthRoutes } from "../app/modules/auth/auth.router";
 import { UserRouter } from "../app/modules/user/user.router";
 import { DoctorRoutes } from "../app/modules/doctor/doctor.router";
 import { AdminRoutes } from "../app/modules/admin/admin.router";
+import { scheduleRoutes } from "../app/modules/schedule/schedule.router";
+import { DoctorScheduleRoutes } from "../app/modules/doctorSchedule/doctorSchedule.router";
 
 
 
@@ -13,5 +15,8 @@ router.use("/specialties",SpecialtyRouter);
 router.use("/users",UserRouter)
 router.use("/doctors",DoctorRoutes)
 router.use("/admins",AdminRoutes)
+router.use("/schedules", scheduleRoutes)
+router.use("/doctor-schedules", DoctorScheduleRoutes)
+// router.use("/appointments", AppointmentRoutes)
 
 export const IndexRoutes=router;
